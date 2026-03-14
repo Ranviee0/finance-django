e7513e92c5788494176769569e5459c3de74f494
Ranviee0 <neodyte@gmail.com>
2026-03-14 10:52:04 +0700

    feat: TransactionType are Income and Expense

--- api/models.py
+++ api/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
+class TransactionType(models.TextChoices):
+    INCOME = "Income", "Income"
+    EXPENSE = "Expense", "Expense"
+
 # Create your models here.
 class FinanceTransaction(models.Model):
-    type = models.CharField(max_length=10)
+    type = models.CharField(max_length=10, choices=TransactionType.choices)
     datetime = models.DateTimeField()
     category = models.CharField(max_length=256)
     notes = models.TextField()
